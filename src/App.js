4502e172161adbb2e8123e524d24837c25b2d48f
Deniz H BEYAZ <denizhbeyaz@gmail.com>
2020-10-04 12:50:38 +0300

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -13,15 +13,61 @@ function App() {
 	}
 
 	const submitHandler = (event) => {
-		const newItem = {name:keyword, id:Date.now()};
-		setToDos([...toDos, newItem]);
+		// axios.post{todoItem}().success
+		// .error
+		setToDos([...toDos, {name:keyword, edit:false}]);
+
+
 		setKeyword('');
 		event.preventDefault();
 	}
 
-	const deleteItem = (itemName) => {
-		setToDos(toDos.filter((item)=>item.name!==itemName));
+	const deleteItem = (x) => {
+		const newTodos = [...toDos];
+		newTodos.splice(x,1);
+		setToDos(newTodos);
 	}
+
+	const upItem = (x) => {
+		if(x===0){
+			return;
+		}else{
+			let arrToMutate = [...toDos];
+			const a = toDos[x];
+			const b = toDos[x-1];
+			arrToMutate[x] = b;
+			arrToMutate[x-1] = a;
+			setToDos(arrToMutate);
+		}
+	}
+	const downItem = (x) => {
+		if(x!==toDos.length-1){
+			let arrToMutate = [...toDos];
+			const a = toDos[x];
+			const b = toDos[x+1];
+			arrToMutate[x] = b;
+			arrToMutate[x+1] = a;
+			setToDos(arrToMutate);
+		}else{
+			return
+		}
+	}
+
+	const editItem = (x) => {
+		let arrToMutate = [...toDos];
+		arrToMutate[x]['edit'] = !toDos[x]['edit'];
+		setToDos(arrToMutate);
+
+	}
+
+	const editChange = (tempName, index) => {
+		let arrToMutate = [...toDos];
+		arrToMutate[index].name = tempName;
+		setToDos(arrToMutate);
+	}
+
+	
+
   return (
 	<div className="App">
 		<header>
@@ -30,7 +76,11 @@ function App() {
 				<button type="submit">ADD</button>
 			</form>
 		</header>
-		<ListItems items={toDos} deleteItem={deleteItem}/>
+		<div id="top-items">
+			<h4>{toDos.length} item added.</h4>
+			<button onClick={()=>setToDos([])}>Delete All</button>
+		</div>
+		<ListItems items={toDos} editChange={editChange} editItem={editItem} deleteItem={deleteItem} upItem={upItem} downItem={downItem} />
 	</div>
   );
 }
--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function ListItems({ items, deleteItem }) {
-    const listItems = items.map(element=>{
-        return <div key={element.id} className="list">
-                    <p>{element.name}
-                        <span onClick={(e)=>deleteItem(e.target.parentElement.textContent)}>
-                        <i className="fa fa-trash" aria-hidden="true"></i></span>
+function ListItems({ items, deleteItem, upItem, downItem, editItem, editChange }) {
+    const listItems = items.map((item, index)=>{
+        return <div key={index} id={index} className="list">
+                    <p>
+                    {item.edit ? <input onKeyDown={e=> e.key === 'Enter' && editItem(index)} onChange={(e)=>editChange(e.target.value, index)} type="text" value={item.name} /> : item.name }
+                        <span>
+                            <i onClick = {(e)=> editItem(index)} className="fa fa-edit"></i>
+                            <i onClick = {(e)=> downItem(index)} className="fa fa-arrow-down" aria-hidden="true"></i>
+                            <i onClick = {(e)=> upItem(index)} className="fa fa-arrow-up" aria-hidden="true"></i>
+                            <i onClick = {(e)=> deleteItem(index)} className="fa fa-trash" aria-hidden="true"></i>
+                        </span>
                     </p>
                 </div>
     })
